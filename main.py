f861ff8ec927380db311d7d54985cd602842c970
Anthony Carno <acarno@vt.edu>
2016-09-29 16:21:50 -0400

    Added ability to specify subset of programs
    Added an optional command line argument "config" that allows the
    user to specify a file that contains a subset of programs in a
    given folder to run.
    
    The format of the file should be one program per line -- e.g. for
    programs 'foo' and 'bar' in a folder containing 'foo', 'bar', and
    'awk', the config file would look like this:
    
        foo
        bar
    
    Note that as of this commit this functionality is NOT tested.

--- main.py
+++ main.py
@@ -24,30 +24,55 @@ def parse_arguments():
                         help='Location of programs to run')
     parser.add_argument('log_folder',
                         help='Location of log files')
+    parser.add_argument('--config',
+                        help='Configuration file containing files to run')
     args = parser.parse_args()
     return args
+
+def profile_program(args, program):
+    """ Runs profiler for a single program """
+    # Create ProgramProfile
+    path = os.path.join(args.prog_folder, program)
+    profile = getattr(program_profile, args.profile)(path)
+
+    # Run profiling
+    outfile = os.path.join(args.log_folder, '{}.output'.format(program))
+    print('Sending program output to {}'.format(outfile))
+    profile.run(outfile)
+
+    # Dump results
+    dumpfile = os.path.join(args.log_folder, '{}.csv'.format(program))
+    print('Dumping gathered data to {}'.format(dumpfile))
+    profile.dump_to_file(dumpfile)
+
+    print(profile)
+    print()
+
+def run_config(args, program_set):
+    """ Runs a subset of all programs in a folder, as specified by user """
+    for program in sorted(os.listdir(args.prog_folder)):
+        if program in program_set:
+            profile_program(args, program)
+
+def run_all(args):
+    """ Runs all programs in a folder """
+    for program in sorted(os.listdir(args.prog_folder)):
+        profile_program(args, program)
 
 def main(args):
 
     # For every program in folder (assuming folder ONLY contains programs)
-    for program in sorted(os.listdir(args.prog_folder)):
-
-        # Create ProgramProfile
-        path = os.path.join(args.prog_folder, program)
-        profile = getattr(program_profile, args.profile)(path)
-
-        # Run profiling
-        outfile = os.path.join(args.log_folder, '{}.output'.format(program))
-        print('Sending program output to {}'.format(outfile))
-        profile.run(outfile)
-
-        # Dump results
-        dumpfile = os.path.join(args.log_folder, '{}.csv'.format(program))
-        print('Dumping gathered data to {}'.format(dumpfile))
-        profile.dump_to_file(dumpfile)
-
-        print(profile)
-        print()
+    if args.config:
+        try:
+            program_subset = []
+            with open(args.config, 'r') as _:
+                program_subset = [line for line in _.readlines()]
+        except OSError as err:
+            print('Problem reading config file: {}'.format(err))
+        else:
+            run_config(args, program_subset)
+    else:
+        run_all(args)
 
 if __name__ == '__main__':
     args = parse_arguments()
